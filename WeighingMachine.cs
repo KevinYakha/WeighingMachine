fec16c5a76bd4300c4d4010df67cea1b19f66c54
KevinYakha <kevin@yakha.eu>
2024-04-22 16:53:29 +0200

    Exercise solved and submited

--- WeighingMachine.cs
+++ WeighingMachine.cs
@@ -2,11 +2,20 @@ using System;
 
 class WeighingMachine
 {
-    // TODO: define the 'Precision' property
-
-    // TODO: define the 'Weight' property
-
-    // TODO: define the 'DisplayWeight' property
-
-    // TODO: define the 'TareAdjustment' property
+    public int Precision { get; }
+    private double _weight;
+    public double Weight 
+    {
+        get { return _weight; }
+        set { _weight = value < 0 ? throw new ArgumentOutOfRangeException() : value; }
+    }
+    public string DisplayWeight
+    {
+        get { return $"{string.Format($"{{0:N{Precision}}}", Weight - TareAdjustment)} kg"; }
+    }
+    public double TareAdjustment { get; set; } = 5.0d;
+    public WeighingMachine(int precision)
+    {
+        Precision = precision;
+            }
 }
